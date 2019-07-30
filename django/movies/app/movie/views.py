d9c88a0432855752aaedc1bc06ad62c55199dbdb
Gustavo Soares <gustavo@papaga.io>
2019-07-30 11:18:23 -0300

    ListView + DetailView

--- django/movies/app/movie/views.py
+++ django/movies/app/movie/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from . import models
+
+
+'''
+Movies
+'''
+class MovieListView(ListView):
+
+    model = models.Movie
+    template_name = 'movie/list.html'
+
+    # def get_queryset(self):
+    #     return models.Movie.objects.filter(year__gte=1995)
+
+'''
+Movie
+'''
+class MovieDetailView(DetailView):
+
+    model = models.Movie
+    template_name = 'movie/detail.html'
